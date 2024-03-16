fe1307b86edd21cbd2315b227aa40cf2b68324e9
lawless-ionizer <143874802+lawless-ionizer@users.noreply.github.com>
2024-03-16 23:29:20 +0530

    Update system_param.m

--- system_param.m
+++ system_param.m
@@ -4,7 +4,7 @@ function [ params ] = system_param()
 
 params.g = 9.81; % acceleration due to gravity
 params.M = 5; % mass of cart in kgs
-params.m = 1; % mass of pole in kgs
+params.m = 1; % mass of bob in kgs
 params.L = 3; % length of pole in metres
 
 end
